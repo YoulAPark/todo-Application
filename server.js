47fd71e691d6b7fcdd9019614afd46ed0353f8c2
ouneno <dbfdk2531@gmail.com>
2023-07-24 01:01:04 +0900

    2023-07-24 01:00 MongoDB 연결 전까지 완료 함 10:27

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended : true}));
 
-app.listen(8080, function(){
-    console.log('listening on 8080')
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/?retryWrites=true&w=majority', function() {
+    app.listen(8080, function(){
+        console.log('listening on 8080')
+        // MongoDB 10:27
+    });
 });
 
-app.get('/beauty', function(요청, 응답) {
-    응답.send('뷰티용품을 쇼핑할 수 있는 페이지입니다.');  
-});
-
-app.get('/', function(요청, 응답) {
-    응답.sendFile(__dirname + '/index.html');  
-});
 
 app.get('/write', function(요청, 응답) {
     응답.sendFile(__dirname + '/write.html');
+}); 
+
+app.post('/add', function(요청, 응답){
+    응답.send('전송완료');
+    console.log(요청.body.title);
+    console.log(요청.body.date);
+    // DB에 저장해주세요
 });
