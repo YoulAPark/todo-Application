159934bae8068df549937a389f2b2c8f35e4f4f6
ouneno <dbfdk2531@gmail.com>
2023-07-24 23:43:58 +0900

    2023-07-24 23:42 MongoDB Error 현재 노드버전 : v19.0.1 다운그레이드 한 후 재커밋 시도 예정
    [Error]
    MongoParseError: URI malformed, cannot be parsed
        at parseConnectionString (/Users/park-youl-a/Desktop/개별공부/todoApplication/node_modules/mongodb/lib/core/uri_parser.js:567:21)
        at QueryReqWrap.callback (/Users/park-youl-a/Desktop/개별공부/todoApplication/node_modules/mongodb/lib/core/uri_parser.js:119:7)
        at QueryReqWrap.onresolve [as oncomplete] (node:internal/dns/callback_resolver:49:10)

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
 const MongoClient = require('mongodb').MongoClient;
-MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/?retryWrites=true&w=majority', function() {
-    app.listen(8080, function(){
-        console.log('listening on 8080')
-        // MongoDB 10:27
-    });
-});
 
+var db;
+MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
+
+if (에러) return console.log(에러)
+  db = client.db('todoapp');
+
+  db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(에러, 결과){
+    console.log('저장완료'); 
+  });
+
+  app.listen(8080, function () {
+    console.log('listening on 8080')
+  });
+});
 
 app.get('/write', function(요청, 응답) {
     응답.sendFile(__dirname + '/write.html');
