2e2f625b100b56d3d602e24234d49ca75751af05
ouneno <dbfdk2531@gmail.com>
2023-07-25 23:55:28 +0900

    2023-07-25 23:53 MongoDB 연결하여 DB 저장 처리 함

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 
 var db;
-MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/todoApplication?retryWrites=true&w=majority', function (에러, client) {
+
+MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/todoApplication?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
 
 if (에러) return console.log(에러)
-  db = client.db('todoapp');
-
-  db.collection('post').insertOne( {이름 : 'John', _id : 100} , function(에러, 결과){
-    console.log('저장완료'); 
-  }); 
+  db = client.db('todoApplication');
 
   app.listen(8080, function () {
     console.log('listening on 8080')
@@ -24,9 +21,10 @@ app.get('/write', function(요청, 응답) {
     응답.sendFile(__dirname + '/write.html');
 }); 
 
-app.post('/add', function(요청, 응답){
-    응답.send('전송완료');
-    console.log(요청.body.title);
-    console.log(요청.body.date);
-    // DB에 저장해주세요
+app.post('/add', function(요청, 응답) {
+  var todo = (요청.body.title);
+  var date = (요청.body.date);
+  db.collection('post').insertOne( { 제목 : todo, 날짜 : date }, function(에러, 결과) {
+    console.log('저장완료');
+  });
 });
