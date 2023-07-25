ae1eaea838a863359c9edc6bb5e9207dc7c8d2b3
ouneno <dbfdk2531@gmail.com>
2023-07-26 01:30:14 +0900

    2023-07-26 01:30 Commit

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.urlencoded({extended : true}));
-
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
+
 
 var db;
 
@@ -21,10 +22,32 @@ app.get('/write', function(요청, 응답) {
     응답.sendFile(__dirname + '/write.html');
 }); 
 
+// '/add' 라는 링크로 접속했을 때
 app.post('/add', function(요청, 응답) {
-  var todo = (요청.body.title);
-  var date = (요청.body.date);
-  db.collection('post').insertOne( { 제목 : todo, 날짜 : date }, function(에러, 결과) {
-    console.log('저장완료');
-  });
+  응답.send('전송완료');
+
+  // counter라는 collection에서 name이 'boardCnt'인 데이터 하나를 찾아주세요 [findOne]
+  db.collection('counter').findOne({name : 'boardCnt'}, function(에러, 결과){
+    // 결과.totalPost
+    console.log("결과 확인 : "+결과.totalPost);
+    // 총개시물갯수라는 변수로 지정함
+    var 총개시물갯수 = 결과.totalPost;
+
+    // write에서 무엇인가를 작성했을 때 실행되는 함수 [insertOne]
+    db.collection('post').insertOne( { _id : 총개시물갯수+1, 제목 : 요청.body.title, 날짜 : 요청.body.date }, function() {
+      console.log('저장완료');
+      // insert를 진행했을 때 totalPost를 1 increment 시켜줘야하므로 해당 updateOne 함수를 실행시킨다 [updateOne]
+      db.collection('counter').updateOne({name : 'boardCnt'},{ $inc : {totalPost:1} },function(에러, 결과){
+        if(에러){return console.log(에러)} // 에러문 잡고자 할 떄
+      });
+    }); 
+  }); 
+  
 });
+
+app.get('/list', function(요청, 응답) {
+  db.collection('post').find().toArray(function(에러, 결과) { // 전체 가져오기
+    console.log(결과);
+    응답.render('list.ejs', { posts : 결과 });
+  });
+}); 
