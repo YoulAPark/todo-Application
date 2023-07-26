7e717fca38b031c2f15dad7564194728b12dbb2d
ouneno <dbfdk2531@gmail.com>
2023-07-27 03:01:13 +0900

    2023-07-27 03:00 delete, 형변환, 에러처리, html파일들 ejs파일로 변환 후 처리 완료

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine', 'ejs');
-
+app.use('/public', express.static('public'));
 
 var db;
 
@@ -18,9 +18,14 @@ if (에러) return console.log(에러)
   });
 });
 
+app.get('/', function(요청, 응답) {
+  응답.render('index.ejs');
+});
+
 app.get('/write', function(요청, 응답) {
-    응답.sendFile(__dirname + '/write.html');
-}); 
+  응답.render('write.ejs');
+});
+
 
 // '/add' 라는 링크로 접속했을 때
 app.post('/add', function(요청, 응답) {
@@ -60,5 +65,13 @@ app.delete('/delete', function(요청, 응답){
     // 응답.status(400).send({ message : '실패했습니다.' }); // 무조건 400 에러 코드 보냄
     console.log('삭제완료');
   });
+});
 
+app.get('/detail/:id', function(요청, 응답){
+    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+        console.log(결과);
+        응답.render('detail.ejs', { data : 결과 });
+        // if(응답.status(200)){return console.log("성공")};
+        if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
+    });    
 });
