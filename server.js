80463a1fc4b204a22ed9d07825cbc64069c305b4
ouneno <dbfdk2531@gmail.com>
2023-07-27 16:46:25 +0900

    2023-07-27 16:46 CRUD 완성

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const app = express();
 const bodyParser = require('body-parser'); 
 app.use(bodyParser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
+
+/* [method-Override]
+설치방식 : npm install method-override | 종류 : Library | 용도 : ejs form 태그에서 put, delete가 되도록 해준다*/
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
+
 
 var db;
 
@@ -24,11 +31,6 @@ app.get('/', function(요청, 응답) {
 
 app.get('/write', function(요청, 응답) {
   응답.render('write.ejs');
-});
-
-
-app.post('/update', function(요청, 응답) {
-  응답.send('update 전송완료');
 });
 
 // '/add' 라는 링크로 접속했을 때
@@ -86,3 +88,30 @@ app.get('/edit/:id', function(요청, 응답) {
     if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
   }); 
 });
+
+app.put('/edit', function(요청, 응답){
+    // 폼에 담긴 제목, 날짜 데이터를 가지고
+    // db.collection에다가 update 함
+  db.collection('post').updateOne( {_id : parseInt(요청.body.id)}, { $set : { 제목 : 요청.body.title, 날짜 : 요청.body.date } }, function(에러, 결과){
+      console.log('수정완료');
+      응답.redirect('/list');
+  });
+});
+
+/* 수정, PUT, [updateOne]
+1) db.collection('post').updateOne( {A},{B}, function(에러, 결과){
+    A라는 데이터를 찾아다가 B라는 데이터로 바꿔달라
+
+2) db.collection('post').updateOne( {A},{ $set : {} }, function(에러, 결과){
+
+3) {_id : 요청.body.id 
+  요청.body.(input type="text" 인 것 중에 name="id" 인 것을 가져와달라.)
+
+4) 완성
+db.collection('post').updateOne( {_id : parseInt(요청.body.id)}, { $set : { 제목 : 요청.body.title, 날짜 : 요청.body.date } }, function(에러, 결과){
+*/
+
+/*
+1. [새로고침]
+응답.redirect('/list');
+*/
