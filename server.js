ffcdf1cc8911fe2da820077b0719c422e4ae7233
ouneno <dbfdk2531@gmail.com>
2023-07-27 17:09:40 +0900

    2023-07-27 17:09 Library 설치 및 회원인증기능 구현

--- server.js
+++ server.js
@@ -7,12 +7,25 @@ const MongoClient = require('mongodb').MongoClient;
 /* [method-Override]
 설치방식 : npm install method-override | 종류 : Library | 용도 : ejs form 태그에서 put, delete가 되도록 해준다*/
 const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method')); 
+
+// [로그인준비]
+/* [passport] [passport-local] [express-session]
+설치방식 : npm install passport passport-local express-session | 종류 : Library | 용도 : Session 로그인 기능 구현*/
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+// Middleware?
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
-
+ 
 var db;
 
 MongoClient.connect('mongodb+srv://admin:qwer1234@jennifer.j22z1ok.mongodb.net/todoApplication?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (에러, client) {
@@ -98,20 +111,6 @@ app.put('/edit', function(요청, 응답){
   });
 });
 
-/* 수정, PUT, [updateOne]
-1) db.collection('post').updateOne( {A},{B}, function(에러, 결과){
-    A라는 데이터를 찾아다가 B라는 데이터로 바꿔달라
-
-2) db.collection('post').updateOne( {A},{ $set : {} }, function(에러, 결과){
-
-3) {_id : 요청.body.id 
-  요청.body.(input type="text" 인 것 중에 name="id" 인 것을 가져와달라.)
-
-4) 완성
-db.collection('post').updateOne( {_id : parseInt(요청.body.id)}, { $set : { 제목 : 요청.body.title, 날짜 : 요청.body.date } }, function(에러, 결과){
-*/
-
-/*
-1. [새로고침]
-응답.redirect('/list');
-*/
+app.post('/login', function(요청, 응답){
+  // (회원인증기능 1) 로그인 페이지 만들기 & 아이디 비번 검사 07:37 
+});
