44e837913c29b839e8b5dcc7a51c3e321673449c
ouneno <dbfdk2531@gmail.com>
2023-08-11 19:57:16 +0900

    2023-08-11 19:57 에러해결 후 commit

--- routes/member.js
+++ routes/member.js
@@ -1,40 +1,20 @@
 var router = require('express').Router();
+const passport = require('passport');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
 
-router.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
-router.use(passport.initialize());
-router.use(passport.session());
+router.get('/join', function(요청, 응답) {
+    응답.render('join.ejs');
+});
 
-passport.use(new LocalStrategy({
-    usernameField: 'id',  // input name 이 id 인 값
-    passwordField: 'pw',  // input name 이 pw 인 값
-    session: true,  // 로그인 후 세션 저장할건지
-}, (input_id, input_pw, done) => {
-    db.collection('login').findOne({id: input_id}, (error, user)=>{
-        if(error) return done(error);
-        if(!user) {
-            console.log('로그인실패: 아디없음');
-            return done(null, false, {message: '존재하지 않는 아이디'});
-        }
-        bcrypt.compare(input_pw, user.pw, (error, result)=>{  // 기존 로그인 확인 코드
-            try {
-                if(result) {
-                    console.log('로그인성공');
-                    return done(null, user);
-                } else {
-                    console.log('로그인실패: 비번틀림');
-                    return done(null, false, {message: '비번틀림'});
-                }
-            } catch(error) {
-                return done(error);
-            }
-        })
-    })
-}));
+router.get('/login', function(요청, 응답){
+    응답.render('login.ejs');
+})
+
+router.get('/myPage', (요청, 응답) => {
+    응답.render('mypage.ejs', {사용자 : 요청.user});
+});
+
 
 router.post('/login', passport.authenticate('local', {
     failureRedirect : '/member/loginFail'
@@ -59,11 +39,9 @@ router.post('/register', async function(요청, 응답){
         console.log('NickName이 이미 존재합니다.');
         응답.send({code : 1});
     } else {
-        console.log('중복아니므로 가입이 가능합니다.');
+        console.log(id + ' 님의 가입완료');
         bcrypt.hash(pw, saltRounds, (에러, hash) => {
-            console.log(hash)
             try {
-                console.log('bcrpt SUCCESS')
                 db.collection('login').insertOne({ id : id, nickName : nickName, pw : hash }, (에러, 결과) => {
                     응답.send({code : 2})
                 });
@@ -72,16 +50,6 @@ router.post('/register', async function(요청, 응답){
             }
         });        
     }
-});
-
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-    db.collection('login').findOne({id: id}, (error, result)=>{
-        done(error, result);
-    })
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,40 @@
+/*[라이브러리 시작]###################################################################################################################################################*/
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); 
-app.use(bodyParser.urlencoded({extended : true}));
 
+// MongoDB
 const MongoClient = require('mongodb').MongoClient;
 
-const methodOverride = require('method-override');
-app.use(methodOverride('_method')); 
+// bodyParser : post방식으로 보낼 때 데이터들을 쉽게 처리해줌
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}));
+
+// ejs : view engine으로 서버데이터를 HTML에 쉽게 넣을 수 있는 HTML 렌더링 엔진
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
+// methodOverride : ejs파일의 <form/> 태그에서 GET/POST 뿐만이 아니라 PUT/DELETE가 되도록 해준다
+const methodOverride = require('method-override');
+app.use(methodOverride('_method')); 
+
+// passport : Login시 session 구현
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+// passport로 미들웨어 생성
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+// dotenv : .env파일 사용 용도
 require('dotenv').config();
   
-
+// multer : 파일전송 및 파일명저장,분석
 let multer = require('multer');
+
+let db;
+
 var storage = multer.diskStorage({
   destination : function(req, file, cb){
     cb(null, './public/image') // 이미지 저장하는 폴더 지정
@@ -31,66 +52,82 @@ var storage = multer.diskStorage({
 limits:{
     fileSize: 1024 * 1024
 }
-}); // diskStorage : 일반 하드 내 저장 / memoryStorage : RAM에 저장(휘발성있음)
+});
+var upload = multer({storage : storage});
 
-var upload = multer({storage : storage}); // => 미들웨어처럼 실행하면 된다.
-
+// socket.io : 채팅
 const http = require('http').createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(http);
 
+// MongoDB에서 ObjectId 사용하고 싶을 때
 const { ObjectId } = require('mongodb'); // ObjectId 사용하고 싶을 때
 
+/*[라이브러리 끝]###################################################################################################################################################*/
+
 // MongoDB 연결
+
 const db_id = process.env.DB_ID;
 const db_pw = process.env.DB_PW;
 const db_cluster = process.env.DB_CLUSTER;
 const server_port = process.env.SERVER_PORT;
 const db_url = "mongodb+srv://" + db_id + ":" + db_pw + "@" + db_cluster + ".j22z1ok.mongodb.net/todoApplication?retryWrites=true&w=majority"
-let db;
-
 MongoClient.connect(db_url, { useUnifiedTopology: true }, function (에러, client) {
   if(에러) {
     return console.log(에러);
   } else {
     app.listen(server_port, ()=>{
+      db = client.db('todoApplication');
       global.db = client.db('todoApplication');
       console.log('<<<<< listening on server >>>>>')
     });
   }
 });
 
-// ROUTER
+// 라우터설정 
 app.use('/member', require('./routes/member.js'));
 
-// NAVIGATION
+// 단순네비게이션
 app.get('/', function(요청, 응답) {
   응답.render('index.ejs');
 });
-
-app.get('/join', function(요청, 응답) {
-  응답.render('join.ejs');
-});
-
-app.get('/login', function(요청, 응답){
-  응답.render('login.ejs');
-})
-
 app.get('/write', function(요청, 응답) {
   응답.render('write.ejs');
 });
-
 app.get('/upload', function(요청, 응답) {
   응답.render('upload.ejs');
 })
 
-//myPage로 들어올경우 로그인했니라는 미들웨어(메서드)를 실행한다
-app.get('/myPage', 로그인했니, function(요청, 응답){
-  응답.render('myPage.ejs', {사용자 : 요청.user})
+/*[로그인관련시작]########################################################################*/
+passport.use(new LocalStrategy({
+  usernameField: 'id',
+  passwordField: 'pw',
+  session: true,
+  passReqToCallback: false,
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+  if (에러) return done(에러)
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+// session을 저장 : 로그인 성공시 발동
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
 });
-
-// 미들웨어 만드는법
-function 로그인했니(요청, 응답, next){ // 요청.user가 있을 경우 next를 실행해라
+// session 찾을 때 실행됨 : myPage 접속시 등
+passport.deserializeUser(function (아이디/*user.id*/, done) {
+  db.collection('login').findOne( {id : 아이디}, function(에러, 결과) {
+    done(null, 결과)
+  });
+});
+// Session사용 : 미들웨어생성
+function 로그인했니 (요청, 응답, next){ //요청.user가 있을 경우 next를 실행해라
   if (요청.user) {
     next()
   } else {
@@ -98,78 +135,23 @@ function 로그인했니(요청, 응답, next){ // 요청.user가 있을 경우 
   }
 }
 
-// 단일 파일 업로드
-app.post('/upload', upload.single('profile'), function(요청, 응답){// upload.single('name에 적힌 명')
+//[파일업로드]
+// 파일업로드(단일) 
+app.post('/uploadSingle', upload.single('profile'), function(요청, 응답){// upload.single('name에 적힌 명')
   응답.send('업로드완료')
 }); 
-
-// 다중 파일 업로드 => upload.array('변수명', 한번에 다운받을 자료의 개수)
-// app.post('/upload', upload.array('profile', 10), function(요청, 응답){// upload.single('name에 적힌 명')
-//   응답.send('업로드완료')
-// }); 
-
-// 이미지 가져올 수 있는 경로 => <img src = "/image/music.jpg"> 
-app.get('/image/:imageName', function(요청, 응답){
+// 다중 파일 업로드
+app.post('/uploadMulti', upload.array('profile', 10), function(요청, 응답){//  => upload.array('변수명', 한번에 다운받을 자료의 개수) / upload.single('name에 적힌 명')
+  응답.send('업로드완료')
+}); 
+// 이미지 경로 
+app.get('/image/:imageName', function(요청, 응답){ // => <img src = "/image/music.jpg"> 
   응답.sendFile( __dirname + '/public/image/' + 요청.params.imageName); // __dirname 현 파일의 경로 server.js
 });
 
-app.get('/search', (요청, 응답)=>{
-  //db.collection('post').find({ $text : { $search : 요청.query.value } }).toArray((에러, 결과)=>{
-  // 해당 부분은 mongoDB에서 Search Index를 적용하는 방법임 => '글쓰기이지만' 이라고 입력해야 검색이 되는 부분에 대한 처리
-  var 검색조건 = [
-    {
-      $search: {
-        index: 'titleSearch',
-        text: {
-          query: 요청.query.value,
-          path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
-        }
-      }
-    },
-    // { $project : { 제목 : 1, _id : 1, score : { $meta : "searchScore" } } }, $meta : 검색이 얼마나 일치하는지 score라는 숫자로 나타내준다.
-    { $sort : { _id : 1 } }, // _id를 기준으로 1 : 오름차순 // -1 : 내림차순
-    { $limit : 10 } // 최대 몇 개 까지 조회할 것인지
-]
-    db.collection('post').aggregate(검색조건).toArray((에러, 결과)=>{
-    console.log(결과)
-    응답.render('search.ejs', {posts : 결과})
-  })
-});
-
-// indexing 처리 - mongoDB에서 indexing 진행 후 사용
-// { $text : ; { $search : 요청.query.value } }
-
-app.get('/list', function(요청, 응답) {
-  db.collection('post').find().toArray(function(에러, 결과) { // 전체 가져오기
-    //console.log(결과);
-    응답.render('list.ejs', { posts : 결과 });
-  });
-}); 
-
-app.get('/detail/:id', function(요청, 응답){
-    db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
-        console.log("!! : "+결과);
-        응답.render('detail.ejs', { data : 결과 });
-        // if(응답.status(200)){return console.log("성공")};
-        if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
-    });    
-});
-
-app.put('/edit', function(요청, 응답){
-    // 폼에 담긴 제목, 날짜 데이터를 가지고
-    // db.collection에다가 update 함
-  db.collection('post').updateOne( {_id : parseInt(요청.body.id)}, { $set : { 제목 : 요청.body.title, 날짜 : 요청.body.date } }, function(에러, 결과){
-      console.log('수정완료');
-      응답.redirect('/list');
-  });
-});
-
-
-// '/add' 라는 링크로 접속했을 때
+// 글쓰기
 app.post('/add', function(요청, 응답) {
   console.log(요청.user._id)
-  // 응답.send('전송완료');
-  // counter라는 collection에서 name이 'boardCnt'인 데이터 하나를 찾아주세요 [findOne]
   db.collection('counter').findOne({name : 'boardCnt'}, function(에러, 결과){
     // 결과.totalPost
     //console.log("결과 확인 : "+결과.totalPost);
@@ -187,7 +169,70 @@ app.post('/add', function(요청, 응답) {
   }); 
   응답.redirect('/list');
 });
- 
+
+// 리스트
+app.get('/list', function(요청, 응답) {
+  db.collection('post').find().toArray(function(에러, 결과) { // 전체 가져오기
+    //console.log(결과);
+    응답.render('list.ejs', { posts : 결과 });
+  });
+}); 
+
+// 검색
+app.get('/search', (요청, 응답)=>{
+  //db.collection('post').find({ $text : { $search : 요청.query.value } }).toArray((에러, 결과)=>{
+  // 해당 부분은 mongoDB에서 Search Index를 적용하는 방법임 => '글쓰기이지만' 이라고 입력해야 검색이 되는 부분에 대한 처리
+  var 검색조건 = [
+    {
+      $search: {
+        index: 'titleSearch',
+        text: {
+          query: 요청.query.value,
+          path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+        }
+      }
+    },
+    // { $project : { 제목 : 1, _id : 1, score : { $meta : "searchScore" } } }, $meta : 검색이 얼마나 일치하는지 score라는 숫자로 나타내준다.
+    { $sort : { _id : 1 } }, // _id를 기준으로 1 : 오름차순 // -1 : 내림차순
+    { $limit : 10 } // 최대 몇 개 까지 조회할 것인지
+  ]
+    db.collection('post').aggregate(검색조건).toArray((에러, 결과)=>{
+    console.log(결과)
+    응답.render('search.ejs', {posts : 결과})
+  })
+});
+// indexing 처리 - mongoDB에서 indexing 진행 후 사용
+// { $text : ; { $search : 요청.query.value } }
+
+// 상세보기
+app.get('/detail/:id', function(요청, 응답){
+  db.collection('post').findOne({ _id : parseInt(요청.params.id) }, function(에러, 결과){
+      console.log("!! : "+결과);
+      응답.render('detail.ejs', { data : 결과 });
+      // if(응답.status(200)){return console.log("성공")};
+      if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
+  });    
+});
+
+// 수정 GET 네비게이션
+app.get('/edit/:id', function(요청, 응답) {
+  var 수정할데이터 = { _id : parseInt(요청.params.id), 작성자 : 요청.user._id }
+  db.collection('post').findOne(수정할데이터,function(에러, 결과){
+    응답.render('edit.ejs', { data : 결과 });
+    if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
+  }); 
+});
+// 수정 POST
+app.put('/edit', function(요청, 응답){
+  // 폼에 담긴 제목, 날짜 데이터를 가지고
+  // db.collection에다가 update 함
+  db.collection('post').updateOne( {_id : parseInt(요청.body.id)}, { $set : { 제목 : 요청.body.title, 날짜 : 요청.body.date } }, function(에러, 결과){
+      console.log('수정완료');
+      응답.redirect('/list');
+  });
+});
+
+// 삭제
 app.delete('/delete', function(요청, 응답){
   요청.body._id = parseInt(요청.body._id); // 문자열로 치환되는 body._id를 숫자로 치환해주는 것
   var 삭제할데이터 = { _id : 요청.body._id, 작성자 : 요청.user._id }
@@ -200,17 +245,16 @@ app.delete('/delete', function(요청, 응답){
   });
 });
 
-app.get('/edit/:id', function(요청, 응답) {
-  var 수정할데이터 = { _id : parseInt(요청.params.id), 작성자 : 요청.user._id }
-  db.collection('post').findOne(수정할데이터,function(에러, 결과){
-    응답.render('edit.ejs', { data : 결과 });
-    if(결과==null){return console.log("400 Bad Error가 발생했습니다.")};        
-  }); 
+
+
+// 채팅 GET 단순 네비게이션
+app.get('/chat', 로그인했니, function(요청, 응답){
+  db.collection('chatroom').find({ member : 요청.user._id }).toArray().then((결과)=>{
+    응답.render('chat.ejs', {data : 결과})
+  });
 });
 
-
-
-// 채팅
+// 채팅룸 POST 생성
 app.post('/chatroom', 로그인했니, function(요청, 응답) {
   var 저장할거 = {
     member : [ObjectId(요청.body.당한사람id), 요청.user._id] ,
@@ -221,12 +265,7 @@ app.post('/chatroom', 로그인했니, function(요청, 응답) {
   });
 });
 
-app.get('/chat', 로그인했니, function(요청, 응답){
-  db.collection('chatroom').find({ member : 요청.user._id }).toArray().then((결과)=>{
-    응답.render('chat.ejs', {data : 결과})
-  });
-});
-
+// 메세지 보내기
 app.post('/message', 로그인했니, function(요청, 응답) {
   var 저장할거 = {
     parent : 요청.body.parent , //상위게시물
